5fed0a9b132b370879da17255bcd0783dde5f326
Andrew Andersen <andersac88@gmail.com>
2018-10-03 20:07:02 -0500

    more DRY

--- assest/javascript/app.js
+++ assest/javascript/app.js
@@ -9,67 +9,66 @@ var unanswered = 0;
 var intervalId;
 //create array of questions and answers; each question is an new object with the incorrect and correct answers being keys
 var questionsAnswers = [{
-    question: "When was the attack on Pearl Harbor?",
-    wrong1: "September 1, 1939",
-    wrong2: "May 7, 1945",
-    wrong3: "December 11, 1941",
-    correct: "December 7, 1941",
+    question:"When was the attack on Pearl Harbor?",
+    answer: ["September 1, 1939",
+            "May 7, 1945",
+            "December 11, 1941", "December 7, 1941"],
     image: "assest/images/Pearl.jpg"
 }, {
     question: "Who was the Supreme Allied Commander in Europe?",
-    wrong1: "Winston Churchill",
-    wrong2: "George S. Patton",
-    wrong3: "Franklin Roosevelt",
-    correct: "Dwight Eisenhower",
+   answer: ["Winston Churchill",
+    "George S. Patton",
+    "Franklin Roosevelt",
+    "Dwight Eisenhower"],
     image: "assest/images/Eisenhower.jpg"
 }, {
     question: "What does D-Day refer too?",
-    wrong1: "The end of the war in Europe",
-    wrong2: "Germany invading Poland",
-    wrong3: "The United states recapturing the Philippines",
-    correct: "The Allied invasion of Normandy",
+    answer: ["The end of the war in Europe",
+    "Germany invading Poland",
+    "The United states recapturing the Philippines",
+    "The Allied invasion of Normandy"],
     image: "assest/images/dday.jpg"
 }, {
     question: "What was the first city that the atomic bomb was dropped on?",
-    wrong1: "Nagasaki",
-    wrong2: "Tokyo",
-    wrong3: "Okinawa",
-    correct: "Hiroshima",
+    answer: ["Nagasaki",
+    "Tokyo",
+    "Okinawa",
+    "Hiroshima"],
     image: "assest/images/Hiroshima.jpeg"
 }, {
     question: "Who was the fascist dictator of Italy?",
-    wrong1: "Adolf Hitler",
-    wrong2: "Napoleon Bonaparte",
-    wrong3: "Amerigo Dumini",
-    correct: "Benito Mussolini",
+    answer: ["Adolf Hitler",
+    "Napoleon Bonaparte",
+    "Amerigo Dumini",
+    "Benito Mussolini"],
     image: "assest/images/Mussolini.jpeg"
 }, {
     question: "What famous author was in Dresden, Germany when it was bombed by the Allies",
-    wrong1: "Ian Fleming",
-    wrong2: "Roald Dahl",
-    wrong3: "Ernest Hemingway",
-    correct: "Kurt Vonnegut",
+    answer: ["Ian Fleming",
+    "Roald Dahl",
+    "Ernest Hemingway",
+    "Kurt Vonnegut"],
     image: "assest/images/Vonnegut.jpeg",
 }, {
     question: "Who was Hitler's mistress?",
-    wrong1: "Bridget von Hammersmark",
-    wrong2: "Magda Goebbels",
-    wrong3: "Traudl Junge",
-    correct: "Eva Braun",
+    answer: ["Bridget von Hammersmark",
+    "Magda Goebbels",
+    "Traudl Junge",
+    "Eva Braun"],
     image: "assest/images/Braun.jpeg",
 }, {
     question: "Who was nicknamed 'The Desert Fox'?",
-    wrong1: "Abdul Munim Wassel",
-    wrong2: "George Patton",
-    wrong3: "Bernard Montgomery",
-    correct: "Erwin Rommel",
+    answer: ["Abdul Munim Wassel",
+    "George Patton",
+    "Bernard Montgomery",
+    "Erwin Rommel"],
     image: "assest/images/Rommel.jpeg",
 }, {
     question: "What was the code-name for the German invasion of the Soviet Union",
-    wrong1: "Operation Sea Lion",
-    wrong2: "Operation Torch",
-    wrong3: "Operation Blue",
-    correct: "Operation Barbarossa",
+    answer: ["Operation Sea Lion",
+    "Operation Torch",
+    "Operation Blue",
+    "Operation Barbarossa"],
     image: "assest/images/Barbarossa.jpeg",
 }
 ];
@@ -125,43 +124,20 @@ function gameIsAfoot () {
 // answers to questiosn should be buttons
 // so not DRY......
 var Question = $("<div>");
-var Wrong1Div = $("<div>");
-var Wrong1 = $("<button>");
-var Wrong2Div = $("<div>");
-var Wrong2 = $("<button>");
-var Wrong3Div = $("<div>");
-var Wrong3 = $("<button>");
-var RightDiv = $("<div>");
-var Right = $("<button>");
-
-Wrong1Div.addClass("row justify-content-center");
-Wrong1.attr("type", "button");
-Wrong1.addClass("btn btn-outline-dark btn-lg mb-2 wrong");
-Wrong2Div.addClass("row justify-content-center")
-Wrong2.attr("type", "button");
-Wrong2.addClass("btn btn-outline-dark btn-lg mb-2 wrong");
-Wrong3Div.addClass("row justify-content-center")
-Wrong3.attr("type", "button");
-Wrong3.addClass("btn btn-outline-dark btn-lg mb-2 wrong");
-RightDiv.addClass("row justify-content-center")
-Right.attr("type", "button");
-Right.addClass("btn btn-outline-dark btn-lg mb-2 right");
-
 Question.text(questionsAnswers[count].question)
-Wrong1.text(questionsAnswers[count].wrong1)
-Wrong2.text(questionsAnswers[count].wrong2)
-Wrong3.text(questionsAnswers[count].wrong3)
-Right.text(questionsAnswers[count].correct)
-
 $(".point").append(Question)
-$(".questionPoint").append(Wrong1Div)
-Wrong1Div.append(Wrong1);
-$(".questionPoint").append(Wrong2Div)
-Wrong2Div.append(Wrong2);
-$(".questionPoint").append(Wrong3Div)
-Wrong3Div.append(Wrong3);
-$(".questionPoint").append(RightDiv)
-RightDiv.append(Right);
+
+for (i=0; i<questionsAnswers[count].answer.length; i++) {
+    var buttonDiv = $("<div>")
+    buttonDiv.addClass("row justify-content-center")
+    var button = $("<button>")
+    button.attr("type", "button");
+    button.attr("data-value", [i])
+    button.addClass("btn btn-outline-dark btn-lg mb-2 answer");
+    button.text(questionsAnswers[count].answer[i])
+    $(".questionPoint").append(buttonDiv)
+    buttonDiv.append(button);
+}
 // randomizes the above elemants
 var ul = document.querySelector('.questionPoint');
 for (var i = ul.children.length; i >= 0; i--) {
@@ -169,7 +145,18 @@ for (var i = ul.children.length; i >= 0; i--) {
 }
 
 // click incorrect answer, somethinh should display signiying incorrect; keep tally of incorrect answer
-$(".wrong").on("click", function() {
+$(".answer").on("click", function() {
+    if ($(this).attr("data-value") === "3") {
+        correct++;
+        $(".btn").remove();
+        var youAreCorrect = $("<div class='mt-3 temp'>");
+        youAreCorrect.text("You are Correct!");
+        $(".point").append(youAreCorrect);
+        $(".image").html("<img src=" + questionsAnswers[count].image + ">");
+        clearInterval(intervalId);
+       setTimeout(nextQuestion, 2000);
+       return;
+    } else if ($(this).attr("data-value") !== "3") {
     incorrect++;
     $(".btn").remove()
     var wrongGuess = $("<div class='mt-3 temp'>")
@@ -182,21 +169,10 @@ $(".wrong").on("click", function() {
     clearInterval(intervalId);
     setTimeout(nextQuestion, 2000);
     return
-
- });
+ }
+}) 
 // click correct answer, something should display signifying correct answer; keep tally of correct answers
-$(".right").on("click", function(){
-     correct++
-     $(".btn").remove()
-     var youAreCorrect = $("<div class='mt-3 temp'>")
-     youAreCorrect.text("You are Correct!")
-     $(".point").append(youAreCorrect)
-     $(".image").html("<img src=" + questionsAnswers[count].image + ">")
-     clearInterval(intervalId);
-    setTimeout(nextQuestion, 2000);
-     return
- });
-}
+};
 // following one of the options (correct, incorrect, unasnwered), move onto next question and restart timer
 function nextQuestion() {
     count++
@@ -204,7 +180,6 @@ function nextQuestion() {
     number = 15;
     timer();
     gameIsAfoot();
-   
 }
 
 // timer ticking down from 30 seconds should display for each question
@@ -238,7 +213,3 @@ $(".initiator").on("click", function() {
    timer();
    gameIsAfoot(); 
 });
-
-
-
-
